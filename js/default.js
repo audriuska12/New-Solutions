726b592e79ec7df900835d3cf40699c12053d110
audriuska12 <audriuska4812@gmail.com>
2017-11-18 21:13:05 +0200

    User class defined, constructor

---
+++ js/default.js
@@ -0,0 +1,27 @@
+﻿mvcApp = angular.module('myApp', ['ngMaterial']);
+
+
+
+(function () {
+
+//----------------------------------------------------------
+    mvcApp.config(function(){ 
+       console.log('config started');
+     });
+
+//----------------------------------------------------------
+    mvcApp.controller('ngCtrl_main', function ($scope) {
+	    console.log('controller started');
+        $scope.submit = function() {
+            var name = $scope.name;
+            var pass = $scope.passw;
+            if(pass.length < 6 || pass=="") alert('Slaptazodis yra per trumpas');
+        }
+    });
+
+//----------------------------------------------------------
+    mvcApp.run([function () { 
+      console.log('run started!');
+    }]);
+
+})();
