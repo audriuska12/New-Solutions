bb99d0e70783f622decaf1348c84e18f72abc354
audriuska12 <audriuska4812@gmail.com>
2017-11-19 21:43:24 +0200

    Fixed for db change

--- vartotojo_rusis.php
+++ vartotojo_rusis.php
@@ -7,7 +7,7 @@
  */
 
 /**
- * Description of darbuotoju_rusis
+ * Description of darbuotojo_pareigos
  *
  * @author audri
  */
@@ -15,29 +15,31 @@ class vartotojo_rusis {
 
     var $id;
     var $pavadinimas;
-    var $stazas;
-    var $profesionalumo_lygis;
-    var $sveikatos_sutrikimai;
 
     public function __construct($data) {
         $this->id = $data['id'];
         $this->pavadinimas = $data['pavadinimas'];
-        $this->stazas = $data['stazas'];
-        $this->profesionalumo_lygis = $data['profesionalumo_lygis'];
-        $this->sveikatos_sutrikimai = $data['sveikatos_sutrikimai'];
     }
 
-    public static function getFromDatabase($id) {
+    public function getFromDatabase($id) {
         $dbc = mysqli_connect(get_cfg_var('dbhost'), get_cfg_var('dbuser'), get_cfg_var('dbpw'), get_cfg_var('dbname'));
-        $sql = $dbc->prepare("SELECT * FROM `vartotojo_rusis` WHERE `id`=?");
+        $sql = $dbc->prepare("SELECT * FROM vartotojo_rusis WHERE `id`=?");
         $sql->bind_param('i', $id);
         $sql->execute();
         $result = $sql->get_result();
-        if ($dbc->affected_rows > 0) {
+        if (mysqli_affected_rows($dbc) > 0) {
             $data = $result->fetch_assoc();
             return new vartotojo_rusis($data);
         }
-        return NULL;
+        return null;
+    }
+
+    public static function addToDatabase($pavadinimas) {
+        $dbc = mysqli_connect(get_cfg_var('dbhost'), get_cfg_var('dbuser'), get_cfg_var('dbpw'), get_cfg_var('dbname'));
+        $sql = $dbc->prepare("INSERT INTO vartotojo_rusis (pavadinimas) VALUES (?)");
+        $sql->bind_param('s', $pavadinimas);
+        $sql->execute();
+        return mysqli_insert_id($dbc);
     }
 
 }
