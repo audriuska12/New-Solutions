59945053dc8f87ec7f775e202df34137515f7aca
audriuska12 <audriuska4812@gmail.com>
2017-11-25 19:32:17 +0200

    Lenteliu implementacijos pradzia

--- darbuotoju_sarasas.php
+++ darbuotoju_sarasas.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="tables.css">
+    </head>
+    <body>
 <?php
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
@@ -8,7 +14,7 @@ $countSamdyti = count($darbuotojaiSamdyti);
 if ($countSamdyti > 0) {
     echo "Pasamdyti darbuotojai:";
     echo("<table>");
-    echo("<tr><th>Vardas</th><th>Pavardė</th><th>Kontaktai</th><th>Dirba nuo</th><th>Alga</th><th>Pozicija</th><th>Pareigos</th><th>Grafikas</th><th>Pastabos</th></tr>");
+    echo("<tr><th>Vardas</th><th>Pavardė</th><th>Kontaktai</th><th>Dirba nuo</th><th>Alga</th><th>Pozicija</th><th>Pareigos</th><th>Grafikas</th><th>Pastabos</th><th></th></tr>");
     for ($i = 0; $i < $countSamdyti; $i++) {
         echo("<tr><td>" . $darbuotojaiSamdyti[$i]->vardas . "</td><td>" . $darbuotojaiSamdyti[$i]->pavarde . "</td><td><a href=\"\" onClick=\"popup('kontaktai.php?id=" . $darbuotojaiSamdyti[$i]->id . "')\">Rodyti</a></td><td>" . $darbuotojaiSamdyti[$i]->dirba_nuo . "</td><td>" . $darbuotojaiSamdyti[$i]->alga . "</td><td>" . $darbuotojaiSamdyti[$i]->getRusis()->pavadinimas . "</td><td><a href=\"\" onClick=\"popup('pareigosRodyti.php?id=" . $darbuotojaiSamdyti[$i]->id . "')\">Rodyti</a><td><a href=\"\" onClick=\"popup('grafikasView.php?id=" . $darbuotojaiSamdyti[$i]->id . "')\">Rodyti</a></td><td><a href=\"\" onClick=\"popup('pastabosViesos.php?id=" . $darbuotojaiSamdyti[$i]->id . "')\">Rodyti</a></td><td><a href=\"atleistiDarbuotoja.php?id=" . $darbuotojaiSamdyti[$i]->id . "\" onclick=\"return confirm('Ar tikrai norite atleisti šį darbuotoją?')\">Atleisti</a></td></tr>");
     }
@@ -22,7 +28,7 @@ $countAtleisti = count($darbuotojaiAtleisti);
 if ($countAtleisti > 0) {
     echo "Atleisti darbuotojai:";
     echo("<table>");
-    echo("<tr><th>Vardas</th><th>Pavardė</th><th>Kontaktai</th><th>Dirba nuo</th><th>Alga</th><th>Pozicija</th></tr>");
+    echo("<tr><th>Vardas</th><th>Pavardė</th><th>Kontaktai</th><th>Dirba nuo</th><th>Alga</th><th>Pozicija</th><th></th></tr>");
     for ($i = 0; $i < $countAtleisti; $i++) {
         echo("<tr><td>" . $darbuotojaiAtleisti[$i]->vardas . "</td><td>" . $darbuotojaiAtleisti[$i]->pavarde . "</td><td><a href=\"\" onClick=\"popup('kontaktai.php?id=" . $darbuotojaiAtleisti[$i]->id . "')\">Rodyti</a></td><td>" . $darbuotojaiAtleisti[$i]->dirba_nuo . "</td><td>" . $darbuotojaiAtleisti[$i]->alga . "</td><td>" . $darbuotojaiAtleisti[$i]->getRusis()->pavadinimas . "</td><td><a href=\"atsamdytiDarbuotoja.php?id=" . $darbuotojaiAtleisti[$i]->id . "\" onclick=\"return confirm('Ar tikrai norite atsamdyti šį darbuotoją?')\">Atsamdyti</a></td></tr>");
     }
@@ -41,3 +47,5 @@ if ($countAtleisti > 0) {
         return false;
     }
 </script>
+    </body>
+</html>
